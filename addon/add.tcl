8db9817b3bc493a6aa999dc0ab81a56cf5615909
Thomas Kluge <th.kluge@me.com>
2018-08-14 20:27:04 +0200

    added auto variable creation

--- addon/add.tcl
+++ addon/add.tcl
@@ -5,7 +5,7 @@
 #  https://github.com/thkl/HM-SoundTouch
 
 
-
+load tclrega.so
 source /usr/local/addons/soundtouch/lib/ini.tcl
 source /usr/local/addons/soundtouch/lib/log.tcl
 
@@ -23,7 +23,19 @@ proc checkConfig {} {
   }
 }
 
-
+proc addVariable {name type subtype} {
+  ::log::log info "creating variable $name at your ccu"
+  set rega ""
+  append rega "if (!dom.GetObject(\"$name\")) {\n"
+  append rega "object vars = dom.GetObject( ID_SYSTEM_VARIABLES );\n"
+  append rega "object newVar = dom.CreateObject( OT_VARDP );\n"
+  append rega "newVar.Name(\"$name\");\n"
+  append rega "newVar.ValueType($type);\n"
+  append rega "newVar.ValueSubType($subtype);\n"
+  append rega "vars.Add(newVar.ID());\n"
+  append rega "}"
+  rega_script $rega
+}
 
 ### MAIN
   ::log::log info "HM Soundtouch"
@@ -42,6 +54,10 @@ if {[info exists ip]} {
 			::log::log info "added $i to config"
 			ini::set $config $i "ip_address" $ip
 			ini::commit $config
+			addVariable "BOSE_POWER_$i" "2" "2"
+			addVariable "BOSE_STATE_$i" "20" "11"
+			addVariable "BOSE_TRACK_$i" "20" "11"
+			addVariable "BOSE_VOLUME_$i" "4" "0"
 		} else {
 			::log::log info "$i exists allready in config"
 		}
