0c73eef6559da1966f1184ffcb79623075680c3a
Thomas Kluge <th.kluge@me.com>
2018-08-17 18:52:34 +0200

    fixed addon install bug

--- pack_addon.sh
+++ pack_addon.sh
@@ -4,7 +4,7 @@ version=$(cat VERSION)
 addon_file="$(pwd)/hm-soundtouch.tar.gz"
 tmp_dir=$(mktemp -d)
 
-for f in VERSION update_script update_addon soundtouch addon www lib; do
+for f in VERSION update_script update_addon soundtouch addon www lib etc; do
 	[ -e  $f ] && cp -a $f "${tmp_dir}/"
 done
 chmod 755 "${tmp_dir}/update_script"
